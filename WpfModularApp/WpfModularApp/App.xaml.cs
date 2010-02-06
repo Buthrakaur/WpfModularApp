7e4b84d368f7b14626908997d2fdd27803790dd7
unknown <Filip@.(none)>
2010-02-06 16:05:55 +0100

    nacitani modulu

--- WpfModularApp/WpfModularApp/App.xaml.cs
+++ WpfModularApp/WpfModularApp/App.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using Caliburn.PresentationFramework.ApplicationModel;
 using Castle.Windsor;
 using System.Reflection;
+using System.IO;
 
 namespace WpfModularApp
 {
@@ -22,9 +23,60 @@ namespace WpfModularApp
             return new Caliburn.Castle.WindsorAdapter(container);
         }
 
+        private Assembly[] LoadModules()
+        {
+            var directory = GetCurrentDirectory();
+
+            Func<string, Assembly> getModuleAssembly = (file) => 
+            {
+                try
+                {
+                    return Assembly.LoadFile(file);
+                }
+                catch { }
+                return null;
+            };
+
+            Func<Assembly, IWindsorInstaller> getInstaller = (asm) => {
+                return
+                (from t in asm.GetTypes()
+                 where typeof(IWindsorInstaller).IsAssignableFrom(t)
+                 select (IWindsorInstaller)Activator.CreateInstance(t)).FirstOrDefault();
+            };
+
+            var possibleModules =
+                from file in Directory.GetFiles(directory, "WpfModularApp.*.dll")
+                let asm = getModuleAssembly(file)
+                where asm != null
+                select asm;
+
+            var installers =
+                from m in possibleModules
+                let i = getInstaller(m)
+                where i != null
+                select i;
+
+            container.Install(installers.ToArray());
+
+            return (from i in installers
+                    select i.GetType().Assembly).Distinct().ToArray();
+        }
+
+        private static string GetCurrentDirectory()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            var directory = Path.GetDirectoryName(path);
+            return directory;
+        }
+
         protected override System.Reflection.Assembly[] SelectAssemblies()
         {
-            return base.SelectAssemblies();
+            var modules = LoadModules();
+            return base.SelectAssemblies()
+                .Concat(modules)
+                .ToArray();
         }
 
         protected override object CreateRootModel()
